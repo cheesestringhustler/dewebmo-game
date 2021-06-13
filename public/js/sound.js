3b3aa726e771db276079ffae5cb9151d54027983
cheesestringhustler <manuel8ld@gmail.com>
2021-06-13 16:37:11 +0200

    volume

--- public/js/sound.js
+++ public/js/sound.js
@@ -3,6 +3,11 @@ const walkAudio = new Audio('/sounds/walk.wav');
 const thwompAudio = new Audio('/sounds/thwomp.mp3');
 const deathAudio = new Audio('/sounds/death.mov');
 
+walkAudio.volume = 0.1;
+thwompAudio.volume = 0.1;
+deathAudio.volume = 0.1;
+jumpAudio.volume = 0.1;
+
 function playAudio(name) {
     if (name === 'jump') {
         jumpAudio.play();
